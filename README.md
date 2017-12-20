a338dc0a37ae00c1c5dde0b6523d00f52d981b5e
alexp <aprivalov@wpcalmon01.wpninc.com>
2017-12-20 14:01:23 -0700

    sync from local repo

---
+++ README.md
@@ -0,0 +1,61 @@
+# zbx-nginx
+
+Zabbix Template for Nginx statistics. The script is written in Python.
+
+## System requirements
+
+- [python](http://www.python.org/downloads/)
+- [nginx](http://nginx.org/) with configured `http_stub_status_module`
+
+## Features
+
+- HTTP/HTTPS support
+- Connection Statistics:
+  - Active
+  - Reading
+  - Waiting
+  - Writing
+- Request Statistics:
+  - Accepted
+  - Handled
+  - Total
+- 'Requests Statistics' graph
+- 'Connection Status' graph
+- Screen combining both graphs
+- Version information
+- `{$NGINX_HOST}` and `{$NGINX_PORT}` macros for customization
+
+## Installation
+
+### Nginx Configuration
+
+Add the following to your default vhost configuration file:
+
+```conf
+    location /nginx_status {
+        stub_status on;
+        access_log   off;
+        allow 127.0.0.1;
+        deny all;
+    }
+```
+
+### Zabbix Configuration
+
+1) Copy `userparameter_nginx.conf` to `/etc/zabbix/zabbix_agentd.d` folder (or whatever is default and/or configured on your system).
+
+1) Copy `nginx-stat.py` to `/etc/zabbix/zabbix_agentd.scripts` folder.
+IMPORTANT: if you use another folder for agent scripts, then update userparameter file in the previous step!
+
+1) Import XML template file (`zbx_template_nginx.xml`) into Zabbix via Web GUI (Configuration -> Templates -> Import).
+
+1) Assign the imported template to a host and enjoy!
+
+## Troubleshooting
+
+A simple run of the script from a console should give just one number in return, for example:
+
+```shell
+> /etc/zabbix/zabbix_agentd.scripts/nginx-stat.py -h localhost -p 443 -a active
+14
+```
---
+++ nginx-stat.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+#
+# Options:
+#
+# -a active
+# -a accepted
+# -a handled
+# -a requests
+# -a reading
+# -a writting
+# -a waiting
+#
+
+import sys
+import getopt
+import urllib2
+import re
+import ssl
+
+
+def usage():
+    print "usage: nginx-stat.py -h 127.0.0.1 -p 80 -a [active|accepted|handled|request|reading|writting|waiting]"
+    sys.exit(2)
+
+
+def main():
+
+    # Default values
+    host = "localhost"
+    port = "80"
+    getInfo = "None"
+    proto = "http"
+    _headers = {}
+    gcontext = ""
+
+
+    if len(sys.argv) < 2:
+        usage()
+
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], "h:p:a:")
+    except getopt.GetoptError:
+        usage()
+
+    # Assign parameters as variables
+    for opt, arg in opts:
+        if opt == "-h":
+            host = arg
+        if opt == "-p":
+            port = arg
+        if opt == "-a":
+            getInfo = arg
+
+    if port == "443":
+        proto = "https"
+        _headers = {'X-Mashape-Key': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'}
+        gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+
+    url = proto + "://" + host + ":" + port + "/nginx_status/"
+    request = urllib2.Request(url, headers=_headers)
+    result = urllib2.urlopen(request, context=gcontext)
+
+    buffer = re.findall(r'\d{1,8}', result.read())
+
+## Format:
+## Active connections: 196
+## server accepts handled requests
+## 272900 272900 328835
+## Reading: 0 Writing: 6 Waiting: 190
+
+    if getInfo == "active":
+        print buffer[0]
+    elif getInfo == "accepted":
+        print buffer[1]
+    elif getInfo == "handled":
+        print buffer[2]
+    elif getInfo == "requests":
+        print buffer[3]
+    elif getInfo == "reading":
+        print buffer[4]
+    elif getInfo == "writting":
+        print buffer[5]
+    elif getInfo == "waiting":
+        print buffer[6]
+    else:
+        print "unknown"
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
